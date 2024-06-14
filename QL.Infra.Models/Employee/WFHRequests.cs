50398727d24574b0bfc62c38d5d8bd291b300831
Madhusudhan Kasarapu <madhusudhan.kasarapu@qentelli.com>
2024-06-14 17:18:55 +0530

    swagger UI dded

--- QL.Infra.Models/Employee/WFHRequests.cs
+++ QL.Infra.Models/Employee/WFHRequests.cs
@@ -12,7 +12,7 @@ namespace QL.Infra.Models.Employee
         public required string EmployeeId { get; set; }
         public DateTime Fromdate { get; set; }
         public DateTime Todate { get; set; }
-        public string Status { get; set; }
+        public string? Status { get; set; }
         public string? Comments { get; set; }
         public int No_Of_Days { get; set; }
         public DateTime Approved_Date { get; set; }
--- UserService.API/Program.cs
+++ UserService.API/Program.cs
@@ -6,11 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<IEmployeeWFHRequest, EmployeeWFHRequest>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
