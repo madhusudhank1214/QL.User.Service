e37da4df62a96a06a5e28866e9732df342040f30
Madhusudhan Kasarapu <madhusudhan.kasarapu@qentelli.com>
2024-06-25 16:20:14 +0530

    imaster information repo added

---
+++ QL.Infra.Repository/Repositories/IMasterInfomation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL.Infra.Repository.Repositories
+{
+    public interface IMasterInfomation
+    {
+        string Name { get; }
+
+    }
+}
